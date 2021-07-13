5fbf22ee98860b55aff13d6d5c97da9dd36457bb
Kirill Ki <kirillkalapov@gmail.com>
2021-07-13 14:16:39 +0200

    in progress

--- CarouselEffect/ViewController.swift
+++ CarouselEffect/ViewController.swift
@@ -8,12 +8,49 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    
+    @IBOutlet weak var redLight: UIView!
+    
+    @IBOutlet weak var yellowLight: UIView!
 
+    @IBOutlet weak var greenLight: UIView!
+    
+    
+    @IBOutlet weak var startButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        redLight.backgroundColor = .red
+        yellowLight.backgroundColor = .yellow
+        greenLight.backgroundColor = .green
+        
+        redLight.alpha = 0.3
+        yellowLight.alpha = 0.3
+        greenLight.alpha = 0.3
+        
+        startButton.layer.cornerRadius = 18
+        redLight.layer.cornerRadius = 60
+        yellowLight.layer.cornerRadius = 60
+        greenLight.layer.cornerRadius = 60
+        
+        view.backgroundColor = #colorLiteral(red: 0.9568627477, green: 0.6588235497, blue: 0.5450980663, alpha: 1)
+        
+        
+        
+        
+        
+        
+       
     }
 
 
+    @IBAction func startButtonPressed(_ sender: UIButton) {
+    }
+    
+    
+    
+    
 }
+
 
